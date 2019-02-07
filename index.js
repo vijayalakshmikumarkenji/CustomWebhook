66a5740a69191143593476433f01f71ce308f503
vijayalakshmikk <44222121+vijayalakshmikk@users.noreply.github.com>
2019-02-07 12:46:24 +0530

    dynamic user input5

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ restService.post('/echo', (req, res) => {
             req.body.result.parameters.email
             ? req.body.result.parameters.email
             : "Seems like some problem. No user exists on DB";
-    console.log("email from req:" + req.body.result.parameters.email);
+    console.log("email from req:" + email_id);
  //   console.log("username from req:" + req.body.result.parameters.username);
 
     var target_url = "https://sb.ftdmobileapi.com/user/exists?email=" + email_id + "&uid=9MFPAH0OROD6VDEWEWQWTZYNB5NKML467RXO9WDMS9MIL122RM&type=android&appversion=11.0.0&app=sharisberries_android&design=1&scale=3.0";
