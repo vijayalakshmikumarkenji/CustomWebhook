d5b06d23f805dca8019dd4c69b9f8759544d19f5
vijayalakshmikk <44222121+vijayalakshmikk@users.noreply.github.com>
2019-02-08 12:30:17 +0530

    feb8

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { actionssdk } = require('actions-on-google');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const restService = express();
 const request = require('request-promise-native');
-var username="";
-
+var username = "";
+const utilConstants = require('utilConstants');
 restService.use(
     bodyParser.urlencoded({
         extended: true
@@ -17,38 +17,51 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
-    //console.log('nouvelle requete');
-    //console.log(req.body);
+
     const app = actionssdk();
     const agent = new WebhookClient({ request: req, response: res });
-    const email_intent = 'intent.emailid';
-    const user_name = 'intent.username';
     let intentMap = new Map();
-    intentMap.set(email_intent, handleEmailidRequest);
-    intentMap.set(user_name, handleUsernameRequest);
+    intentMap.set('intent.emailid', handleEmailidRequest);
+    intentMap.set('intent.username', handleUsernameRequest);
+    intentMap.set('intent.emailid - yes', handleTypeOfGiftToOrder)
     agent.handleRequest(intentMap);
 
 });
 
+//When user says yes to the question "Do you want to buy any gift" asked in intent.emailid
+function handleTypeOfGiftToOrder(agent) {
+    console.log("Enter handleTypeOfGiftToOrder :" + utilConstants.base_url);
+    agent.add("Select something from below gifts \n");
 
-function handleUsernameRequest(agent){
+    var options = {
+        uri: "https://sb.ftdmobileapi.com/product/list?uid=9MFPAH0OROD6VDEWEWQWTZYNB5NKML467RXO9WDMS9MIL122RM&type=android&appversion=11.0.0&app=sharisberries_android&design=1&scale=3.0",
+        json: true
+    };
+
+    request.get(options).then(result => {
+        console.log("product lists json :" + result);
+        console.log("product lists json :" + JSON.parse(result).length);
+
+        return Promise.resolve(agent);
+    });
+
+
+}
+
+function handleUsernameRequest(agent) {
     console.log("username :" + agent.parameters.username);
     username = agent.parameters.username;
-    agent.add("Hi "+username+"Can I check whether Are you an existing user");
+    agent.add("Hi " + username + "Can I check whether Are you an existing user");
     return Promise.resolve(agent);
 }
 
 
 function handleEmailidRequest(agent) {
-
     console.log("email :" + agent.parameters.email);
-    console.log("username :" +username);
-   
+    console.log("username :" + username);
     var email_id = agent.parameters.email;
-
     var options = {
-        uri: "https://sb.ftdmobileapi.com/user/exists?email=" + email_id + "&uid=9MFPAH0OROD6VDEWEWQWTZYNB5NKML467RXO9WDMS9MIL122RM&type=android&appversion=11.0.0&app=sharisberries_android&design=1&scale=3.0"
-        ,
+        uri: "https://sb.ftdmobileapi.com/user/exists?email=" + email_id + "&uid=9MFPAH0OROD6VDEWEWQWTZYNB5NKML467RXO9WDMS9MIL122RM&type=android&appversion=11.0.0&app=sharisberries_android&design=1&scale=3.0",
         json: true
     };
 
@@ -56,7 +69,7 @@ function handleEmailidRequest(agent) {
         .then(result => {
             console.log(result.reference);
             if (result.reference == "ACCOUNT_EXISTS") {
-                agent.add("Hi "+ username+" you are already exist on FTD world :) Welcome :) How may I help you??");
+                agent.add("Hi " + username + " you are already exist on FTD world :) Welcome :) Do you want to buy any gift??");
             } else {
                 agent.add("You are new to FTD. Can I create an account for you");
             }
---
+++ utilConstants.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var base_url= "https://sb.ftdmobileapi.com/";
+var user_exists_url = "user/exists?email=";
+exports.user_exists_url;
+exports.base_url;
